5456e991b748bc29d1d5ea72c24de0c10eca34d3
hammadali03 <ham223006@gmail.com>
2024-06-12 17:13:56 +0500

    add some

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -10,7 +10,7 @@ const Todo = () => {
 const add=()=>{
    const inputText=inputRef.current.value.trim()
 //    console.log(inputText)
-if(inputRef===""){
+if(inputText===""){
     return null
 }
         const newTodo={
@@ -44,7 +44,7 @@ useEffect(()=>{
 },[todolist])
 
   return (
-    <div className="bg-white place-self-center max-w-md flex flex-col p-7 w-5/12 min-h-[500px] rounded-lg">
+    <div className="bg-white place-self-center max-w-md flex flex-col md:p-7 p-3 w-6/12 min-h-[550px] rounded-lg">
      {/* ----- title ----- */}
 
      <div className="flex mt-7 gap-2 items-center">
@@ -53,9 +53,12 @@ useEffect(()=>{
      </div>
 
      {/* ----- input box ----- */}
-     <div className="flex items-center my-7 bg-gray-200  rounded-full">
-        <input type="text" ref={inputRef} placeholder="Add your Task" className="bg-transparent border-0 outline-none md:h-12 h-8 pl-6 pr-2 placeholder:text-slate-600"/>
-        <button  onClick={add} className=" cursor-pointer bg-orange-600 w-14 md:w-32 md:h-14 h-8 text-white rounded-full border-none md:text-lg text-xs font-medium">ADD +</button>
+     <div className="flex items-center my-7 bg-gray-200 w-fit  md:ml-10 ml-2 rounded-full">
+        <input type="text" ref={inputRef} placeholder="Add your Task" className="bg-transparent border-0 outline-none  pl-3 placeholder:text-slate-600"/>
+        <button  onClick={add} className=" cursor-pointer bg-orange-600 px-4 py-3 text-white rounded-full border-none md:text-lg text-xs font-medium whitespace-nowrap">        ADD  +
+
+        
+        </button>
      </div>
 
      {/* ---- todo list----- */}
